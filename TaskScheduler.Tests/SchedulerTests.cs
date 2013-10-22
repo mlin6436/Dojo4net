34763254a30ab50928bbf7a7e2c2959821c5e7ed
Meng Lin <mlin6436@gmail.com>
2013-10-23 00:22:20 +0100

    add test with in-depth task dependencies

--- TaskScheduler.Tests/SchedulerTests.cs
+++ TaskScheduler.Tests/SchedulerTests.cs
@@ -69,5 +69,23 @@ namespace TaskScheduler.Tests
                 Assert.That(result[i].Name, Is.EqualTo(expected[i].Name));
             }
         }
+
+        [Test]
+        public void GetTaskSchedulingResult_ShouldReturnScheduledTaskListWithTaskADependsOnTaskBAndTaskBDependsTaskC()
+        {
+            var a = new Task("a");
+            var b = new Task("b");
+            var c = new Task("c");
+            a.Dependency = b;
+            b.Dependency = c;
+            var input = new List<Task>() { a, b, c };
+            var expected = new List<Task>() { c, b, a };
+            var scheduler = new Scheduler();
+            var result = scheduler.GetTaskSchedulingResult(input);
+            for (int i = 0; i < input.Count; i++)
+            {
+                Assert.That(result[i].Name, Is.EqualTo(expected[i].Name));
+            }
+        }
     }
 }
